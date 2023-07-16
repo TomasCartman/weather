cda326127b8e6c3b0ce25bff0c87e7bb63afd2f7
Marco Rios <marco12y@outlook.com>
2023-07-16 19:02:27 -0300

    removed unused components

--- frontend/src/components/ComponentWrapper.jsx
+++ frontend/src/components/ComponentWrapper.jsx
@@ -3,7 +3,7 @@ import './ComponentWrapper.css'
 export default function ComponentWrapper(props) {
     return (
         <div className={props.className}>
-            <div className={`wrapper ${props.innerClassName}`}>
+            <div className={`wrapper`}>
                 {props.children}
             </div>
         </div>
